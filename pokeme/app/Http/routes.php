074ba71de23177ac62bce5cbbc02870a81b60f6a
Osamu Fujimoto <oaf7862@rit.edu>
2016-03-05 15:55:36 -0500

    initial commit

---
+++ pokeme/app/Http/routes.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('login/fb', function() {
+    $facebook = new Facebook(Config::get('facebook'));
+    $params = array(
+        'redirect_uri' => url('/login/fb/callback'),
+        'scope' => 'email',
+    );
+    return Redirect::to($facebook->getLoginUrl($params));
+});
+
+Route::get('login/fb/callback', function() {
+    $code = Input::get('code');
+    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+
+    $facebook = new Facebook(Config::get('facebook'));
+    $uid = $facebook->getUser();
+
+    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+
+    $me = $facebook->api('/me');
+
+    $profile = Profile::whereUid($uid)->first();
+    if (empty($profile)) {
+
+        $user = new User;
+        $user->name = $me['first_name'].' '.$me['last_name'];
+        $user->email = $me['email'];
+        $user->photo = 'https://graph.facebook.com/'.$me['username'].'/picture?type=large';
+
+        $user->save();
+
+        $profile = new Profile();
+        $profile->uid = $uid;
+        $profile->username = $me['username'];
+        $profile = $user->profiles()->save($profile);
+    }
+
+    $profile->access_token = $facebook->getAccessToken();
+    $profile->save();
+
+    $user = $profile->user;
+
+    Auth::login($user);
+
+    return Redirect::to('/')->with('message', 'Logged in with Facebook');
+});
+
+Route::get('/', function()
+{
+    $data = array();
+
+    if (Auth::check()) {
+        $data = Auth::user();
+    }
+    return View::make('user', array('data'=>$data));
+});
+
+Route::get('logout', function() {
+    Auth::logout();
+    return Redirect::to('/');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
---
+++ pokeme/config/facebook.php
@@ -0,0 +1,8 @@
+<?php
+// app/config/facebook.php
+
+// Facebook app Config 
+return array(
+        'appId' => '862313447211670',
+        'secret' => '7df3b2041823068015eaa4d6f8abe35e'
+    );
